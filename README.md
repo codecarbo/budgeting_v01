1d3c0c395d59dc294273e8b68c744c74da1cefe1
Stephan <stephan.michelfeit@gmx.net>
2024-03-20 19:41:33 +0100

    Created function for csv import

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 15.03.24
 found duplicates not dropped error:
   when saving to csv, pandas omits 2nd zero after decimal point (eg. 100,00 -> 100,0), but only at first save, subsequent saves work (100,00)
-could not find out why, try reading csv simpler (use format provided by online banking (";" as separators, "," in values)
+could not find out why, try reading csv simpler, use format provided by online banking (";" as separators, "," in values)
 
 16.03.24
 simplify csv reading/handling (see above)
@@ -11,4 +11,6 @@ TODO:
   refactor, put code in import function
   simple calculations, have a look at time series data
 
-  
+20.03.24
+create import function (refactor code, put into function)
+think about / create basic evaluation functions (e.g. expenses for given month or within time frame)
---
+++ data_import.py
@@ -0,0 +1,65 @@
+import pandas as pd
+
+
+RELEVANT_COLS = ['IBAN Auftragskonto',
+                 'Valutadatum',
+                 'Name Zahlungsbeteiligter',
+                 'Verwendungszweck',
+                 'Betrag',
+                 'Saldo nach Buchung']
+
+PATH_DATA_ALL = 'data/output/data_all.csv'
+
+
+def import_banking_csv(path):
+
+    data_import = pd.read_csv(path, sep=';', on_bad_lines='warn')
+    
+    # Drop irrelevant columns
+    # # data_new = data_import[relevant_cols] is only a selection!
+    data_new = data_import.drop(data_import.columns.difference(RELEVANT_COLS), axis=1)  # axis=1 means columns
+
+    count_rows_imported = len(data_new.index)
+
+    # Format date
+    data_new['Valutadatum'] = pd.to_datetime(data_new['Valutadatum'], format='%d.%m.%Y', errors='coerce')
+    # # 'coerce': no errors for empty cells
+
+    data_all = pd.read_csv(PATH_DATA_ALL, index_col=0, parse_dates=['Valutadatum'])
+    # # index_col = 0: read first column as index column
+
+    count_rows_existing = len(data_all.index)
+
+    # Create temporary concatination
+    data_tmp = pd.concat([data_all, data_new], axis=0, ignore_index=True)
+    # # axis=0 (along rows) is default = combine rows of input tables
+    # # ignore_index=True: ignore existing index, set new index for all
+
+    count_rows_tmp = len(data_tmp.index)
+
+    data_all = data_tmp.drop_duplicates()
+
+    count_rows_all = len(data_all.index)
+
+    count_rows_dropped = count_rows_tmp - count_rows_all
+
+    count_rows_new = count_rows_imported - count_rows_dropped
+
+    # Sort by date, newest first
+    data_all = data_all.sort_values(by='Valutadatum', ascending=False)
+
+    data_all = data_all.reset_index(drop=True)
+
+    data_all.to_csv("data/output/data_all.csv")  # index=False: not writing unnamed first column
+
+    # Count information to return
+    row_counts = {
+        'existing': count_rows_existing,
+        'imported': count_rows_imported,
+        'tmp': count_rows_tmp,
+        'dropped': count_rows_dropped,
+        'new': count_rows_new,
+        'all': count_rows_all
+    }
+
+    return row_counts
